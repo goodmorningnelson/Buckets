9cea52e88040ecbbd3ff5f7b22f4b587c721fb84
Thomas Ruehr <thomasruehr@Thomass-MacBook-Pro.local>
2026-03-13 16:57:37 +0100

    Change background color to blue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div style={{
       height: '100%',
-      backgroundColor: '#ffa900',
+      backgroundColor: 'blue',
     }}>
       <div style={{
         display: 'flex',
